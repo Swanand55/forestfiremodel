e8788c0c2838effb01577d5cb07c510847792039
Swanand55 <swanand30201@gmail.com>
2025-06-19 09:10:10 +0530

    Beanstalk configuration

--- New folder/application.py
+++ New folder/application.py
@@ -5,26 +5,44 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
-
 application = Flask(__name__)
-app = application
+app=application
 
-## import ridgre regressor and std scaler pickle
-
-ridge_model = pickle.load(open('models/ridge.pkl','rb'))
-standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
-
+## import ridge regressor and std scaler pickle
+ridge_model = pickle.load(open('29_lifecycle_of_ml_project/ridge.pkl', 'rb'))
+stdscl_model = pickle.load(open('29_lifecycle_of_ml_project/scaler.pkl', 'rb'))
+regeression_model = pickle.load(open('29_lifecycle_of_ml_project/regression.pkl', 'rb'))
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/predictor', methods=['GET','POST'])
-def predict_datapoint():
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predictdata():
     if request.method=="POST":
-        pass
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        Classes = float(request.form.get('Classes'))
+        Region = float(request.form.get('Region'))
+
+        new_data_scaled = stdscl_model.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result = ridge_model.predict(new_data_scaled)
+
+        return render_template('home.html', results=result[0])
+
     else:
         return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
+
+
+
+
